b588ca9fa4983bb1d94bb31975d3fca951632ead
“mayluv17” <mayowaibrahim009@gmail.com>
2023-12-09 21:38:50 +0100

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/funkids/",
   plugins: [react(), tsconfigPaths()]
+
   // resolve: {
   //   alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   // },
